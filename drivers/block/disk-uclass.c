40368558ed0e737834071b3f315401eb3bab6413
Mugunthan V N <mugunthanvnm@ti.com>
2016-04-17 19:00:46 +0530

    drivers: block: disk-uclass: implement scsi_init()
    Implement scsi_init() api to probe driver model based sata
    devices.
    
    Signed-off-by: Mugunthan V N <mugunthanvnm@ti.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- drivers/block/disk-uclass.c
+++ drivers/block/disk-uclass.c
@@ -7,6 +7,45 @@
 
 #include <common.h>
 #include <dm.h>
+#include <dm/uclass-internal.h>
+#include <dm/device-internal.h>
+#include <scsi.h>
+
+int scsi_get_device(int index, struct udevice **devp)
+{
+	struct udevice *dev;
+	int ret;
+
+	ret = uclass_find_device(UCLASS_DISK, index, &dev);
+	if (ret || !dev) {
+		printf("%d device not found\n", index);
+		return ret;
+	}
+
+	ret = device_probe(dev);
+	if (ret) {
+		error("device probe error\n");
+		return ret;
+	}
+
+	*devp = dev;
+
+	return ret;
+}
+
+void scsi_init(void)
+{
+	struct udevice *dev;
+	int ret;
+
+	ret = scsi_get_device(0, &dev);
+	if (ret || !dev) {
+		error("scsi device not found\n");
+		return;
+	}
+
+	scsi_scan(1);
+}
 
 UCLASS_DRIVER(disk) = {
 	.id		= UCLASS_DISK,
