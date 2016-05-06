SunnyGeGe/myir-u-boot

commit ca8630a640e76a13682a24b2595628105929e088

ARM: omap5: Add config for board/cpu fdt fixups
Adds CONFIG_OF_BOARD_SETUP to the config header files
for dra7xx_evm and am57xx_evm.

Signed-off-by: Daniel Allred <d-allred@ti.com>
Signed-off-by: Madan Srinivas <madans@ti.com>

include/configs/am57xx_evm.h
@@ -20,6 +20,9 @@
 
 #define CONFIG_BOARD_EARLY_INIT_F
 
+/* Linux interfacing, DTB/FDT fixups */
+#define CONFIG_OF_BOARD_SETUP
+
 #define CONFIG_NR_DRAM_BANKS		2
 
 #define CONFIG_ENV_SIZE			(64 << 10)

include/configs/dra7xx_evm.h
@@ -15,6 +15,9 @@
 #define CONFIG_DRA7XX
 #define CONFIG_BOARD_EARLY_INIT_F
 
+/* Linux interfacing, DTB/FDT fixups */
+#define CONFIG_OF_BOARD_SETUP
+
 #ifdef CONFIG_SPL_BUILD
 #define CONFIG_IODELAY_RECALIBRATION
 #endif

commit e0e297488f7625ede7895d9d54a8a3818f8cfc5b

ARM: DRA7: Move CONFIG_OF_BOARD_SETUP to defconfigs
Commit ca8630a640e7 ("ARM: omap5: Add config for board/cpu fdt
fixups") added the CONFIG_OF_BOARD_SETUP in the SoC config header
files for AM57xx and DRA7xx SoCs. All such Kconfig options should
be enabled in defconfig files, so move the CONFIG_OF_BOARD_SETUP
from the SoC config header files to defconfig files for AM57xx
and DRA7xx platforms.

Fixes: ca8630a640e7 ("ARM: omap5: Add config for board/cpu fdt fixups")
Signed-off-by: Suman Anna <s-anna@ti.com>

configs/am57xx_evm_defconfig
@@ -36,3 +36,4 @@ CONFIG_FIT=y
 CONFIG_SPL_OF_LIBFDT=y
 CONFIG_SPL_LOAD_FIT=y
 CONFIG_OF_LIST="am57xx-beagle-x15 am572x-idk am571x-idk"
+CONFIG_OF_BOARD_SETUP=y

configs/am57xx_evm_nodt_defconfig
@@ -23,3 +23,4 @@ CONFIG_CMD_FAT=y
 CONFIG_CMD_FS_GENERIC=y
 CONFIG_SYS_NS16550=y
 CONFIG_OF_LIBFDT=y
+CONFIG_OF_BOARD_SETUP=y

configs/am57xx_hs_evm_defconfig
@@ -33,3 +33,4 @@ CONFIG_DM_MMC=y
 CONFIG_SPI_FLASH=y
 CONFIG_SPI_FLASH_BAR=y
 CONFIG_SYS_NS16550=y
+CONFIG_OF_BOARD_SETUP=y

configs/dra7xx_evm_defconfig
@@ -58,3 +58,4 @@ CONFIG_G_DNL_MANUFACTURER="Texas Instruments"
 CONFIG_G_DNL_VENDOR_NUM=0x0451
 CONFIG_G_DNL_PRODUCT_NUM=0xd022
 CONFIG_OF_LIBFDT=y
+CONFIG_OF_BOARD_SETUP=y

configs/dra7xx_evm_nodt_defconfig
@@ -41,3 +41,4 @@ CONFIG_G_DNL_MANUFACTURER="Texas Instruments"
 CONFIG_G_DNL_VENDOR_NUM=0x0451
 CONFIG_G_DNL_PRODUCT_NUM=0xd022
 CONFIG_OF_LIBFDT=y
+CONFIG_OF_BOARD_SETUP=y

configs/dra7xx_hs_evm_defconfig
@@ -58,3 +58,4 @@ CONFIG_USB_GADGET_DOWNLOAD=y
 CONFIG_G_DNL_MANUFACTURER="Texas Instruments"
 CONFIG_G_DNL_VENDOR_NUM=0x0451
 CONFIG_G_DNL_PRODUCT_NUM=0xd022
+CONFIG_OF_BOARD_SETUP=y

include/configs/am57xx_evm.h
@@ -20,9 +20,6 @@
 
 #define CONFIG_BOARD_EARLY_INIT_F
 
-/* Linux interfacing, DTB/FDT fixups */
-#define CONFIG_OF_BOARD_SETUP
-
 #define CONFIG_NR_DRAM_BANKS		2
 
 #define CONFIG_ENV_SIZE			(64 << 10)

include/configs/dra7xx_evm.h
@@ -15,9 +15,6 @@
 #define CONFIG_DRA7XX
 #define CONFIG_BOARD_EARLY_INIT_F
 
-/* Linux interfacing, DTB/FDT fixups */
-#define CONFIG_OF_BOARD_SETUP
-
 #ifdef CONFIG_SPL_BUILD
 #define CONFIG_IODELAY_RECALIBRATION
 #endif