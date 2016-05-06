4fa301aa8187fb38e846bda2f133ecbdeea00853
Roger Quadros <rogerq@ti.com>
2016-05-06 13:45:46 +0530

    keystone2: move platform specific NAND configs to platform specific header
    Not all keystone 2 devices use Davinci NAND controller. Move the platform
    specific NAND configurations into platform specific headers.
    
    Signed-off-by: Roger Quadros <rogerq@ti.com>
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>
    Signed-off-by: Mugunthan V N <mugunthanvnm@ti.com>

--- include/configs/k2e_evm.h
+++ include/configs/k2e_evm.h
@@ -27,13 +27,21 @@
 	"name_uboot=u-boot-spi-k2e-evm.gph\0"				\
 	"name_fs=arago-console-image-k2e-evm.cpio.gz\0"
 
+/* NAND Configuration */
+#define CONFIG_SYS_NAND_PAGE_2K
+#define CONFIG_NAND_DAVINCI
+#define CONFIG_KEYSTONE_RBL_NAND
+#define CONFIG_KEYSTONE_NAND_MAX_RBL_SIZE	CONFIG_ENV_OFFSET
+#define CONFIG_SYS_NAND_MASK_CLE		0x4000
+#define CONFIG_SYS_NAND_MASK_ALE		0x2000
+#define CONFIG_SYS_NAND_CS			2
+#define CONFIG_SYS_NAND_4BIT_HW_ECC_OOBFIRST
+#define CONFIG_SYS_NAND_LARGEPAGE
+
 #include <configs/ti_armv7_keystone2.h>
 
 /* SPL SPI Loader Configuration */
 #define CONFIG_SPL_TEXT_BASE           0x0c100000
-
-/* NAND Configuration */
-#define CONFIG_SYS_NAND_PAGE_2K
 
 /* Network */
 #define CONFIG_KSNET_NETCP_V1_5
