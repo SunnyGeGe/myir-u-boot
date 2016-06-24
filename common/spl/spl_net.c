dec7a1216ac1c1b69575419e6405710acda5dbd1
Mugunthan V N <mugunthanvnm@ti.com>
2016-06-24 18:06:34 +0530

    common: spl: net: add support for fit images
    Add support for fit images when loading via ethernet/usb rndis
    
    Signed-off-by: Mugunthan V N <mugunthanvnm@ti.com>

--- common/spl/spl_net.c
+++ common/spl/spl_net.c
@@ -11,17 +11,36 @@
 #include <errno.h>
 #include <spl.h>
 #include <net.h>
+#include <dm.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
+#ifndef CONFIG_SPL_LOAD_FIT_ADDRESS
+#define CONFIG_SPL_LOAD_FIT_ADDRESS 0
+#endif
+
+static ulong spl_net_load_read(struct spl_load_info *load, ulong sector,
+			       ulong count, void *buf)
+{
+	debug("%s: sector %lx, count %lx, buf %lx\n",
+	      __func__, sector, count, (ulong)buf);
+	memcpy(buf, (void *)(CONFIG_SPL_LOAD_FIT_ADDRESS + sector), count);
+	return count;
+}
+
 int spl_net_load_image(const char *device)
 {
+	struct image_header *header;
 	int rv;
 
 	env_init();
 	env_relocate();
 	setenv("autoload", "yes");
-	load_addr = CONFIG_SYS_TEXT_BASE - sizeof(struct image_header);
+	if (IS_ENABLED(CONFIG_SPL_LOAD_FIT))
+		load_addr = CONFIG_SPL_LOAD_FIT_ADDRESS;
+	else
+		load_addr = CONFIG_SYS_TEXT_BASE - sizeof(struct image_header);
+
 	rv = eth_initialize();
 	if (rv == 0) {
 		printf("No Ethernet devices found\n");
@@ -34,7 +53,20 @@ int spl_net_load_image(const char *device)
 		printf("Problem booting with BOOTP\n");
 		return rv;
 	}
-	spl_parse_image_header((struct image_header *)load_addr);
+
+	header = (struct image_header *)load_addr;
+
+	if (IS_ENABLED(CONFIG_SPL_LOAD_FIT) &&
+	    image_get_magic(header) == FDT_MAGIC) {
+		struct spl_load_info load;
+
+		debug("Found FIT\n");
+		load.bl_len = 1;
+		load.read = spl_net_load_read;
+		spl_load_simple_fit(&load, 0, header);
+	} else {
+		spl_parse_image_header((struct image_header *)load_addr);
+	}
 
 	return 0;
 }
